cad947be2cecae1766eac013f4043d037c650237
Mahadi Hasan <12mahadihasan@gmail.com>
2024-10-14 12:16:07 +0600

    Project Initialized

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customColor: '#1D4ED8', // Replace with your desired color value
+        // You can add more custom colors here
+      },
+    },
+  },
+  plugins: [],
+}
+
