65b3d74b4ea07f064b98b46efa7d6ae6e38322b3
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-10-15 22:33:22 +0600

    Removed unused codes

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,7 +2,6 @@ import { LiaShippingFastSolid } from "react-icons/lia";
 import { MdOutlineDiscount } from "react-icons/md";
 import { RiSecurePaymentLine } from "react-icons/ri";
 import { RxReload } from "react-icons/rx";
-import bookThumbnail from "./book.webp";
 
 export const footerData = [
   {
@@ -57,57 +56,6 @@ export const advertisementData = [
     icon: RiSecurePaymentLine,
     title: "Secure Payment",
     description: "100% Secure Payment",
-  },
-];
-
-export const booksData = [
-  {
-    title: "The Great Gatsby",
-    totalDownload: 1025,
-    author: "F. Scott Fitzgerald",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "1984",
-    totalDownload: 899,
-    author: "George Orwell",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "To Kill a Mockingbird",
-    totalDownload: 1099,
-    author: "Harper Lee",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    totalDownload: 1199,
-    author: "J.K. Rowling",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "The Hobbit",
-    totalDownload: 599,
-    author: "J.R.R. Tolkien",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "Pride and Prejudice",
-    totalDownload: 605,
-    author: "Jane Austen",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "The Catcher in the Rye",
-    totalDownload: 1299,
-    author: "J.D. Salinger",
-    thumbnail: bookThumbnail,
-  },
-  {
-    title: "The Alchemist",
-    totalDownload: 999,
-    author: "Paulo Coelho",
-    thumbnail: bookThumbnail,
   },
 ];
 
